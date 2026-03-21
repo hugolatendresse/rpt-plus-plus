d25da4a5aa92e3c055729c8f3eea343bdac87741
Hugo Latendresse <hugolatendresse@gmail.com>
2026-03-21 19:09:56 +0000

    revert 100 interleaved 5 times

--- scripts/measure/100_cold_interleaved.sql
+++ scripts/measure/100_cold_interleaved.sql
@@ -20,10 +20,6 @@ SET rpt_forward_only = true;
 -- SET disable_tiered_hash_cache = true;
 ----------------------------------------------
 
-
-
-
-
 -- https://duckdb.org/docs/stable/dev/profiling
 PRAGMA enable_profiling = 'json';
 PRAGMA profiling_output = 'scripts/measure/100_cold_interleaved.json';
@@ -72,7 +68,27 @@ ORDER BY random();
 ANALYZE a;
 ANALYZE b;
 
--- EXPLAIN ANALYZE 
+PREPARE benchmark_query AS
 SELECT min(b.valueB1) 
 FROM a 
 JOIN b ON a.keyB1 = b.keyB1;
+
+-- Warmup: prime the OS page cache.
+EXECUTE benchmark_query;
+
+.print Running the 100_cold_interleaved benchmark query
+SET VARIABLE t0 = epoch_ms(now());
+.timer on
+EXECUTE benchmark_query;
+EXECUTE benchmark_query;
+EXECUTE benchmark_query;
+EXECUTE benchmark_query;
+EXECUTE benchmark_query;
+.timer off
+SET VARIABLE t5 = epoch_ms(now());
+
+.print Show the detailed timed query plan
+.output stdout
+EXPLAIN ANALYZE EXECUTE benchmark_query;
+
+SELECT printf('Average run time: %.3f s', (getvariable('t5') - getvariable('t0')) / 5.0 / 1000.0) AS info;
