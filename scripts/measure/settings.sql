a95f315f315924c42008fc76a5680fbf24964448
Hugo Latendresse <hugolatendresse@gmail.com>
2026-04-01 00:37:11 +0000

    usual settings

--- scripts/measure/settings.sql
+++ scripts/measure/settings.sql
@@ -1,5 +1,5 @@
 SET max_temp_directory_size='0KiB';
-SET threads = 1;
+SET threads = 32;
 -- Statistics propagation sometimes just removes the cold portion
 SET disabled_optimizers = 'compressed_materialization,join_order,build_side_probe_side,statistics_propagation';
 SET thc_collect_phase_rows = 400_000;
