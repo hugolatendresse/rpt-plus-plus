19f04150e67ec38b5737880f3213f19cefe46d7b
Hugo Latendresse <hugolatendresse@gmail.com>
2026-04-12 01:57:15 +0000

    ingore result.json

--- scripts/measure/settings-common.sql
+++ scripts/measure/settings-common.sql
@@ -2,10 +2,10 @@
 
 SET thc_activation_threshold = 1_000_000;
 SET thc_collect_budget_fraction = 0.25;
--- SET thc_collect_phase_rows = 100_000;
-SET thc_collect_phase_rows = 8192;
--- SET thc_first_read_only_phase_rows = 100_000;
-SET thc_first_read_only_phase_rows = 8192;
+SET thc_collect_phase_rows = 100_000;
+-- SET thc_collect_phase_rows = 8192;
+SET thc_first_read_only_phase_rows = 100_000;
+-- SET thc_first_read_only_phase_rows = 8192;
 -- SET thc_l3_budget = 33_554_432; -- 32M
 SET thc_l3_budget = 37_748_736; -- 36MB
 -- SET thc_l3_budget = 62_914_560; -- 60MB
@@ -14,7 +14,7 @@ SET thc_miss_below_which_skip_collect = 0.1;
 
 
 SET max_temp_directory_size='0KiB';
-SET threads = 64;
+SET threads = 1;
 SET disable_perfect_hashing = true;
 SET pin_threads = 'on';
 
