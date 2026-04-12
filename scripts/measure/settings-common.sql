114566b53b0c2b266788b0f81386985ce04a2b11
Hugo Latendresse <hugolatendresse@gmail.com>
2026-04-12 01:52:21 +0000

    restore full size of THC

--- scripts/measure/settings-common.sql
+++ scripts/measure/settings-common.sql
@@ -2,10 +2,12 @@
 
 SET thc_activation_threshold = 1_000_000;
 SET thc_collect_budget_fraction = 0.25;
-SET thc_collect_phase_rows = 10_000;
-SET thc_first_read_only_phase_rows = 10_000;
+-- SET thc_collect_phase_rows = 100_000;
+SET thc_collect_phase_rows = 8192;
+-- SET thc_first_read_only_phase_rows = 100_000;
+SET thc_first_read_only_phase_rows = 8192;
 -- SET thc_l3_budget = 33_554_432; -- 32M
-SET thc_l3_budget = 1_748_736; -- 36MB
+SET thc_l3_budget = 37_748_736; -- 36MB
 -- SET thc_l3_budget = 62_914_560; -- 60MB
 -- SET thc_l3_budget = 67_108_864; -- 64MB
 SET thc_miss_below_which_skip_collect = 0.1;
