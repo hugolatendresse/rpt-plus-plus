903721582c7ec1a5d9902d1edc3828ed66171907
Hugo Latendresse <hugolatendresse@gmail.com>
2026-05-13 02:20:29 +0000

    100k start works better for 8 threads

--- scripts/measure/settings-common.sql
+++ scripts/measure/settings-common.sql
@@ -3,8 +3,8 @@
 -- THC
 SET thc_activation_threshold = 1_000_000;
 SET thc_collect_budget_fraction = 1.00;
-SET thc_collect_phase_rows = 1_000_000;
--- SET thc_collect_phase_rows = 100_000;
+-- SET thc_collect_phase_rows = 1_000_000;
+SET thc_collect_phase_rows = 100_000;
 -- SET thc_collect_phase_rows = 8192;
 SET thc_first_read_only_phase_rows = 100_000;
 -- SET thc_first_read_only_phase_rows = 100_000;
@@ -19,26 +19,27 @@ SET thc_miss_below_which_skip_collect = 0.0;
 
 -- General Parameters
 SET max_temp_directory_size='0KiB';
-SET threads = 1;
-SET disable_perfect_hashing = true;
+SET threads = 8;
 SET pin_threads = 'on';
 
 -- Populate fine-grained hash-join timers (Build Time, Probe Time, Match Time,
 -- THC Collect/Insert/Probe Time, ...) in the DuckDB profiling JSON. 
-SET enable_hash_join_timers = true;
+SET enable_hash_join_timers = false;
 
 
 -- Optimizer
-SET transfer_graph_seed = 0;
-SET use_seeded_root = false;
-SET use_seeded_transfer_order = true;
-SET join_order_mode = 'seeded_left_deep'; -- Is what we want in the end and does not cause the issue
-SET allow_build_probe_side_swap = false; -- Is what we want in the end and does not cause the issue
-SET skip_unfiltered_tables_create_bf_plan = false; -- Run RPT+ logic to not create BF for tables with filters (during CreateBloomFilterPlan) // Is what we want in the end and does not cause the issue
-SET skip_unfiltered_tables_graph_creation = false; -- Same as above but during TransferGraphCreation // CULPRIT!!!!
-SET drop_bf_at_runtime = false; -- Give up BF creation at runtime due to selectivity or memory usage // Is what we want in the end and does not cause the issue
+-- SET disable_perfect_hashing = true;
+-- SET transfer_graph_seed = 0;
+-- SET use_seeded_root = false;
+-- SET use_seeded_transfer_order = true;
+-- SET join_order_mode = 'seeded_left_deep'; -- Is what we want in the end and does not cause the issue
+-- SET allow_build_probe_side_swap = false; -- Is what we want in the end and does not cause the issue
+-- SET skip_unfiltered_tables_create_bf_plan = false; -- Run RPT+ logic to not create BF for tables with filters (during CreateBloomFilterPlan) // Is what we want in the end and does not cause the issue
+-- SET skip_unfiltered_tables_graph_creation = false; -- Same as above but during TransferGraphCreation // CULPRIT!!!!
+-- SET drop_bf_at_runtime = false; -- Give up BF creation at runtime due to selectivity or memory usage // Is what we want in the end and does not cause the issue
 
 -- -- NEW OPTIMIZER
+-- SET disable_perfect_hashing = true;
 -- SET transfer_graph_seed = 0;
 -- SET use_seeded_root = false;
 -- SET use_seeded_transfer_order = true;
@@ -49,11 +50,12 @@ SET drop_bf_at_runtime = false; -- Give up BF creation at runtime due to selecti
 -- SET drop_bf_at_runtime = false; -- Give up BF creation at runtime due to selectivity or memory usage
 
 -- -- OLD OPTIMIZER
--- SET transfer_graph_seed = 0;
--- SET use_seeded_root = false;
--- SET use_seeded_transfer_order = false;
--- SET join_order_mode = 'duckdb';
--- SET allow_build_probe_side_swap = true;
--- SET skip_unfiltered_tables_create_bf_plan = true; -- Run RPT+ logic to not create BF for tables with filters (during CreateBloomFilterPlan)
--- SET skip_unfiltered_tables_graph_creation = true; -- Same as above but during TransferGraphCreation
--- SET drop_bf_at_runtime = true; -- Give up BF creation at runtime due to selectivity or memory usage
+SET disable_perfect_hashing = false;
+SET transfer_graph_seed = 0;
+SET use_seeded_root = false;
+SET use_seeded_transfer_order = false;
+SET join_order_mode = 'duckdb';
+SET allow_build_probe_side_swap = true;
+SET skip_unfiltered_tables_create_bf_plan = true; -- Run RPT+ logic to not create BF for tables with filters (during CreateBloomFilterPlan)
+SET skip_unfiltered_tables_graph_creation = true; -- Same as above but during TransferGraphCreation
+SET drop_bf_at_runtime = true; -- Give up BF creation at runtime due to selectivity or memory usage
