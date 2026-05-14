b779f125130d2fc2ca50dd50c346013f31b046cc
Hugo Latendresse <hugolatendresse@gmail.com>
2026-05-14 03:04:58 +0000

    task to move results

--- scripts/measure/run_rptplus_paper_benchmarks.sh
+++ scripts/measure/run_rptplus_paper_benchmarks.sh
@@ -6,6 +6,6 @@
 # To detach: Ctrl-b then d
 # Later: tmux attach -t bench
 
-scripts/measure/run_appian.sh --cases 1,2,3,4 --duckdb-profiling
-scripts/measure/run_job.sh --cases 1,2,3,4 --duckdb-profiling
-scripts/measure/run_tpc.sh --sf 100 --tpch-only --cases 1,2,3,4 --duckdb-profiling
+scripts/measure/run_appian.sh --cases 1,2,3,4 --duckdb-profiling --seeds 20
+scripts/measure/run_job.sh --cases 1,2,3,4 --duckdb-profiling --seeds 20
+scripts/measure/run_tpc.sh --sf 100 --tpch-only --cases 1,2,3,4 --duckdb-profiling --seeds 20 
