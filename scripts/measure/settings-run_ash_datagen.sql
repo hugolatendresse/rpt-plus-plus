f30e5808c58af0447de2327cfe336b84cc20e0c7
Hugo Latendresse <hugolatendresse@gmail.com>
2026-06-29 04:18:24 +0000

    latest ASH-datagen stuff

--- scripts/measure/settings-run_ash_datagen.sql
+++ scripts/measure/settings-run_ash_datagen.sql
@@ -3,7 +3,11 @@
 -- Add SET/SET VARIABLE statements here when needed.
 
 -- statistics propagation removes the cold portion 
-SET disabled_optimizers = 'statistics_propagation';
+SET disabled_optimizers = 'join_order,build_side_probe_side,statistics_propagation';
+
+-- TODO ASH-datagen needs those those things once they exist
+-- SET skip_unfiltered_tables_graph_creation = true;
+-- SET thc_mu_s_method = 'none';
 
 SET VARIABLE scale_factor = 40_000;
 
